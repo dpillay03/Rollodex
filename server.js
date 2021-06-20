448560c8aeb5a1950827c049e85f57dfa9da3375
dpillay03 <dpillay03@gmail.com>
2021-06-20 13:55:16 -1000

    mongo setup, routes are working on Postman

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("The API is working!");
 });
+
+// Define Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
